457c9add1a6dc946fb2fee7d3540e33d514db74e
Victor Lucas <victorlucas@MacBook-Air-de-Victor.local>
2024-06-26 01:04:33 -0400

    feat: configurando modulo de jwt e passport

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { PrismaService } from './prisma/prisma.service'
 import { CreateUserController } from './controllers/create-user.controller'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: (input) => envSchema.parse(input),
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [CreateUserController],
   providers: [PrismaService],
