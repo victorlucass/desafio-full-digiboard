ac67ff69e7339efea6ed68f6cf324592e08d2534
Victor Lucas <victorlucas@MacBook-Air-de-Victor.local>
2024-06-25 23:38:12 -0400

    feat: mudando configurações do env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateUserController } from './controllers/create-user.controller'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (input) => envSchema.parse(input),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateUserController],
   providers: [PrismaService],
 })
